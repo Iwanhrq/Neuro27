69edd478b444796d4cd4f3271dfd5816b2d0e453
Ivan Henrique <ivanhenriqueee@outlook.com>
2025-05-03 15:42:37 -0300

    Supabase Example

---
+++ src/constants/supabase.example.ts
@@ -0,0 +1,10 @@
+// Renomeie este arquivo para 'supabase.ts' e substitua os valores abaixo com as credenciais do Supabase que está no Notion
+
+
+// URL do seu projeto Supabase
+// Encontrado em: Dashboard do Supabase -> Configurações do Projeto -> URL do Projeto
+export const supaUrl = 'url do seu projeto supabase'
+export const anonKey = 'chave anonima'
+
+// IMPORTANTE: Nunca compartilhe sua chave secreta do serviço!
+// Este arquivo deve conter apenas a chave anônima que é segura para ser exposta no cliente 
